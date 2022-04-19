27ad7eba12306a71f987552bed33c4cfec172beb
Hung Q Tang <tangquochung2901@gmail.com>
2022-04-19 20:29:11 +0800

    Add style for ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <h1>Let's get started</h1>
-      <p>THis is also visible!</p>
+      <p>This is also visible!</p>
       <ExpenseItem />
     </div>
   );
--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,5 +1,14 @@
+import "./Expenseitem.css";
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2 className="expense-item h2">Car Insurance</h2>
+      </div>
+      <div className="expense-item__price">$400</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;

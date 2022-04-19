b15b271a2ffe36918b32a1c71aaa6e0510217c23
Hung Q Tang <tangquochung2901@gmail.com>
2022-04-19 21:33:00 +0800

    passing date using props and make ExpenseDate component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,42 @@ import "./App.css";
 import ExpenseItem from "./components/Expenseitem";
 
 function App() {
+  const expenses = [
+    {
+      date: new Date(2022, 4, 19),
+      title: "Car Insurance",
+      price: 399,
+    },
+    {
+      date: new Date(2022, 4, 20),
+      title: "health Insurance",
+      price: 199,
+    },
+    {
+      date: new Date(2022, 5, 21),
+      title: "Car Insurance",
+      price: 99,
+    },
+  ];
   return (
     <div>
       <h1>Let's get started</h1>
       <p>This is also visible!</p>
-      <ExpenseItem />
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        price={expenses[0].price}
+      />
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        price={expenses[1].price}
+      />
+      <ExpenseItem
+        date={expenses[2].date}
+        title={expenses[2].title}
+        price={expenses[2].price}  
+      />
     </div>
   );
 }
--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,12 +1,16 @@
 import "./Expenseitem.css";
-function ExpenseItem() {
+import ExpenseDate from "./ExpenseDate";
+function ExpenseItem(props) {
+
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+        <ExpenseDate date={props.date}/>
+
       <div className="expense-item__description">
-        <h2 className="expense-item h2">Car Insurance</h2>
+        <h2 className="expense-item h2">{props.title}</h2>
       </div>
-      <div className="expense-item__price">$400</div>
+      <div className="expense-item__price">${props.price}</div>
     </div>
   );
 }
